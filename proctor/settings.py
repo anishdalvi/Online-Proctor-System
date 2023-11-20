9b5c87cf78e4973f3947dd7e2aa5d42a8a1825de
anishDalvi <anishdalvi2001@gmail.com>
2023-11-21 00:13:03 +0530

    added pymysql in settings.py

--- proctor/settings.py
+++ proctor/settings.py
@@ -100,20 +100,13 @@ DATABASES = {
         'PASSWORD':'',
         'HOST':'localhost', 
         'PORT':'3306',
-        'OPTIONS': {
-            'ssl': {
-                'ca': '/path/to/ca-cert.pem',     # Path to the CA certificate
-                'cert': '/path/to/client-cert.pem',  # Path to the client certificate
-                'key': '/path/to/client-key.pem',    # Path to the client private key
-                'check_hostname': False,   # Set to True to enable hostname checking
-            },
-        }
+        
     }
 }
 
 # added the lines because of openSSL issue
-# pymysql.version_info = (1, 4, 2, "final", 0)
-# pymysql.install_as_MySQLdb()
+pymysql.version_info = (1, 4, 2, "final", 0)
+pymysql.install_as_MySQLdb()
 
 
 
